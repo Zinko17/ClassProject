35497d65858cf2603e71b813794399359f74fe2d
Zinko17 <vovazin2@gmail.com>
2021-05-09 13:10:31 +0600

    added weapon and armor

--- generate_person.py
+++ generate_person.py
@@ -28,7 +28,6 @@ for index,names in enumerate(person_info_dict['name']):
 sword_info_dict = {
     'name':['kleimor', 'bastard', 'bstock', 'espadon', 'flamberg'],
     'sword_type':['rare','legendary','rare','legendary','normal'],
-    'desc':['wfl,Меч дракона','okfoskfor','Меч крутой короче','fksdok'],
     'size':[random.randint(1,10) for i in range(5)],
     'weight':[random.randint(7,15) for i in range(5)],
     'power':[33,64,32,70,17],
@@ -38,7 +37,39 @@ sword_info_dict = {
 }
 
 sword_database = []
-for index,names in enumerate(person_info_dict['name']):
+for index,names in enumerate(sword_info_dict['name']):
+    new_sword = weapon.Sword(name=names,
+                             size=sword_info_dict['size'][index],
+                             weight=sword_info_dict['weight'][index],
+                             power=sword_info_dict['power'][index],
+                             stamina=sword_info_dict['stamina'][index],
+                             health=sword_info_dict['health'][index],
+                             defense=sword_info_dict['defense'][index])
+    sword_database.append(new_sword)
+
+
+
+armor_info_dict = {
+    'name':['leather', 'chain mall', 'metal', 'aluminium', 'steel'],
+    'sword_type':['rare','legendary','rare','legendary','normal'],
+    'size':[random.randint(1,10) for i in range(5)],
+    'weight':[random.randint(7,15) for i in range(5)],
+    'power':[random.randint(0,20) for i in range(5)],
+    'stamina':[random.randint(0,50) for i in range(5)],
+    'health':[random.randint(0,70) for i in range(5)],
+    'defense':[random.randint(20,100) for i in range(5)],
+}
+
+armor_database = []
+for index,names in enumerate(armor_info_dict['name']):
+    new_sword = weapon.Sword(name=names,
+                             size=sword_info_dict['size'][index],
+                             weight=sword_info_dict['weight'][index],
+                             power=sword_info_dict['power'][index],
+                             stamina=sword_info_dict['stamina'][index],
+                             health=sword_info_dict['health'][index],
+                             defense=sword_info_dict['defense'][index])
+    armor_database.append(new_sword)
 
 
 
