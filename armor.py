70c9cabc699bfa35fdfccc42dc59642edd208b6b
Zinko17 <vovazin2@gmail.com>
2021-05-10 15:47:15 +0600

    fixes

--- armor.py
+++ armor.py
@@ -1,6 +1,6 @@
 class Armor:
 
-    def __init__(self,name:str,size:int,weight:int,power:int,
+    def __init__(self,name:str,size:0,weight:0,power:0,
                  stamina=0,health=0,defense=0):
         self.name = name
         self.size = size
